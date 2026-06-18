84e0ffaed7c980aa00489dce8b6e1b805f933bb9
Shahzaib <shahzaib.k.work@gmail.com>
2026-06-18 16:13:27 +0500

    My first Python Project Menu

---
+++ 01_Student_Manager/main.py
@@ -0,0 +1,10 @@
+print("===== STUDENT MANAGER =====")
+print("1. Add Student ")
+print("2. Show Students ")
+print("3. Search Student ")
+print("4. Delete Student ")
+print("5. Exit ")
+print("===================================")
+
+choice = input("Enter Your Choice (1-5):")
+
